a87622f222b5c080f8d357c50817b1de260bf565
Chris Allison <chris.allison@bgch.co.uk>
2017-06-11 11:49:13 +0100

    added integer test

--- unity-startup-windows.sh
+++ unity-startup-windows.sh
@@ -11,6 +11,17 @@
 # set the number of workspaces vertically thus:
 # gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 1
 
+function checkint()
+{
+  xint=$1
+  xname=$2
+  if [[ $xint =~ ^-?[0-9]+$ ]]; then
+    :
+  else
+    echo "$ME: var: '$xname' not an integer: '$xint'" >&2
+    exit 1
+  fi
+}
 ME=${0##*/}
 
 XRANDR=$(which xrandr)
@@ -21,6 +32,11 @@ WMCTRL=$(which wmctrl)
 read hsz vsz < <($XRANDR | sed -n 's/^Screen 0.*, current \([0-9]\+\) x \([0-9]\+\), .*$/\1 \2/p')
 read mhsz mvsz < <($WMCTRL -d | sed -n 's/^.*DG: \([0-9]\+\)x\([0-9]\+\) .*$/\1 \2/p')
 
+checkint $hsz hsz
+checkint $vsz vsz
+checkint $mhsz mhsz
+checkint $mvsz mvsz
+# the checkint function ensures that the integer arithmetic below works without error
 whsz=$(( mhsz / hsz ))
 wvsz=$(( mvsz / vsz ))
 
